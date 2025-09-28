f2223a5eb97a7653eccee466d434cc9075e2c02f
Splatube <splatube123@gmail.com>
2025-09-28 14:04:58 +0100

    jump+dash

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,13 @@
 extends CharacterBody2D
 
+@export_category("Speed")
 @export var speed := 150
+@export var climb_speed := 100
 @export var jump_speed := 300
+var dash = 1
 
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _process(delta):
-	if Input.is_action_just_pressed("Left"):
-		$Sprite.set_flip_h(true)
-	elif Input.is_action_just_pressed("Right"):
-		$Sprite.set_flip_h(false)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,8 +17,24 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = -jump_speed
 
-	# Get the input direction.
-	var direction = Input.get_axis("Left", "Right")
-	velocity.x = direction * speed
+	if Input.is_action_just_pressed("Dash"):
+		dash = 2
+		await get_tree().create_timer(0.25).timeout
+		dash = 1
+
+	if Input.is_action_pressed("Jump") and is_on_wall():
+		var direction = Input.get_axis("Up", "Down")
+		velocity.y = direction * climb_speed
+
+	else:
+		# Get the input direction.
+		var direction = Input.get_axis("Left", "Right")
+		velocity.x = direction * speed * dash
+		
+		if Input.is_action_just_pressed("Left"):
+			$Sprite.set_flip_h(true)
+		elif Input.is_action_just_pressed("Right"):
+			$Sprite.set_flip_h(false)
+	
 		
 	move_and_slide()
